d08d17045a4abfc7f03f1ed519a6f946b83064bc
Thomas Raynal <thomas.raynal2@gmail.com>
2019-10-06 17:00:24 +0200

    disruptor.net 15

---
+++ Inlining/TestInlining.cs
@@ -0,0 +1,147 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DisruptorPlayground.Inlining
+{
+    public static class Program
+    {
+        public interface IDto
+        {
+            byte[] Data { get; }
+        }
+
+
+        public readonly struct Dto : IDto
+        {
+            public Dto(byte[] data)
+            {
+                Data = data;
+            }
+
+            public byte[] Data { get;  }
+        }
+
+        public class Dto2 : IDto
+        {
+            public Dto2(byte[] data)
+            {
+                Data = data;
+            }
+
+            public byte[] Data { get; set; }
+        }
+
+        public class Processor<T> where T : IDto
+        {
+            
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public void ProcessInlined(ref T dto)
+            {
+                byte[] copy = new byte[dto.Data.Length];
+
+                Array.Copy(dto.Data, copy, dto.Data.Length);
+            }
+
+            public void Process(ref T dto)
+            {
+                byte[] copy = new byte[dto.Data.Length];
+
+                Array.Copy(dto.Data, copy, dto.Data.Length);
+            }
+
+
+        }
+
+        [MemoryDiagnoser]
+        public class TestInlining
+        {
+            private readonly Random _rand;
+
+            public TestInlining()
+            {
+                _rand = new Random();
+            }
+
+            [Benchmark(Baseline = true)]
+            public void ClassNotInlined()
+            {
+                var processor = new Processor<Dto2>();
+                byte[] data = new byte[5];
+
+                for (var i = 0; i < 100; i++)
+                {
+                    _rand.NextBytes(data);
+
+                    var dto = new Dto2(data);
+                    processor.Process(ref dto);
+                }
+
+            }
+
+            [Benchmark]
+            public void ClassInlined()
+            {
+                var processor = new Processor<Dto2>();
+                byte[] data = new byte[5];
+
+                for (var i = 0; i < 100; i++)
+                {
+                    _rand.NextBytes(data);
+
+                    var dto = new Dto2(data);
+                    processor.ProcessInlined(ref dto);
+                }
+            }
+
+
+            [Benchmark]
+            public void StructNotInlined()
+            {
+                var processor = new Processor<Dto>();
+                byte[] data = new byte[5];
+
+                for (var i = 0; i < 100; i++)
+                {
+                    _rand.NextBytes(data);
+
+                    var dto = new Dto(data);
+                    processor.Process(ref dto);
+                }
+            }
+
+
+            [Benchmark]
+            public void StructInlined()
+            {
+                var processor = new Processor<Dto>();
+                byte[] data = new byte[5];
+
+                for (var i = 0; i < 100; i++)
+                {
+                    var dto = new Dto(data);
+                    processor.ProcessInlined(ref dto);
+                }
+            }
+
+
+
+
+
+        }
+
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<TestInlining>();
+        }
+
+
+    }
+
+
+}
---
+++ Inlining/TestInlining2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace DisruptorPlayground.Inlining2
+{
+    [StructLayout(LayoutKind.Explicit, Size = 16)]
+    public struct Storage16
+    {
+    }
+
+    //https://medium.com/@ocoanet/benchmarking-adventures-part-1-avoiding-boxing-fc7756385ffb
+    public class Storage<T>
+    {
+        private Storage16 _valueStorage;
+        private object _defaultStorage;
+
+        public Storage(T obj)
+        {
+            if(!RuntimeHelpers.IsReferenceOrContainsReferences<T>() && Unsafe.SizeOf<T>() <= 16)
+            {
+                Unsafe.As<Storage16, T>(ref _valueStorage) = obj;
+            }
+            else
+            {
+                _defaultStorage = obj;
+            }
+        }
+    }
+
+    public static class Program2
+    {
+        public class TestInlining2
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            var i = 12;
+
+            var storage = new Storage<int>(i);
+        }
+
+    }
+}
